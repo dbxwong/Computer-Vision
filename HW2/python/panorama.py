dd9eddf6a6f4250c027b1cc2614e11adbe0d0795
David Wong <40510434+dbxwong@users.noreply.github.com>
2020-10-05 21:42:05 +0800

    updated panorama, compositeH still buggy in planar.H

--- HW2/python/panorama.py
+++ HW2/python/panorama.py
@@ -1,30 +1,54 @@
 
 import numpy as np
 import cv2
-from matchPics import matchPics
-from helper import plotMatches
 from opts import get_opts
-from planarH import computeH
+from planarH import compositeH
 
 opts = get_opts()
 
 #load images
-#img1 = cv2.imread('../data/pano_left.jpg')
-#img2 = cv2.imread('../data/pano_right.jpg') 
+img1 = cv2.imread('../data/pano_left.jpg')
+img2 = cv2.imread('../data/pano_right.jpg') 
+img_left = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+img_right = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
-img1 = cv2.imread('D:/MRSD/0.4 Computer Vision (16-720AC)/HW2/data/pano_left.jpg')
-img2 = cv2.imread('D:/MRSD/0.4 Computer Vision (16-720AC)/HW2/data/pano_right.jpg') 
+#reference: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html
+# initiate ORB star detector and find keypoints
+orb = cv2.ORB_create()
+keypts1, des1 = orb.detectAndCompute(img_left,None)
+keypts2, des2 = orb.detectAndCompute(img_right,None)
 
-#convert to grayscale
+# Initiate SIFT detector - alternative
+#sift = cv2.SIFT()
 
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+# find the keypoints and descriptors with SIFT - alternative
+#kp1, des1 = sift.detectAndCompute(img1,None)
+#kp2, des2 = sift.detectAndCompute(img2,None)
 
-# sanity check for sufficient match points
-#matches, locs1, locs2 = matchPics(img1, img2, opts)
-#plotMatches(img1, img2, matches, locs1, locs2) #many matches identified, but takes long to compute
 
-stitcher = cv2.createStitcher(try_use_gpu=False)  #true = use GPU to aid stitching
-pano_output = stitcher.stitch((img1,img2)) ##SOME ERROR HERE
-cv2.imshow("output", pano_output)
-cv2.waitKey(0)
+# create BFMatcher object - reference: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_matcher/py_matcher.html
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# match descriptors
+matches = bf.knnMatch(des1,des2,k=1)
+
+good = []
+for m,n in matches:
+    if m.distance < 0.03*n.distance:
+        good.append(m)
+
+min_num_matches = 10
+if len(good)>min_num_matches:
+    matched_locs1 = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    matched_locs2 = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    # compute homography
+    H1to2, mask = cv2.findHomography(matched_locs1, matched_locs2, cv2.RANSAC, ransacReprojThreshold=2.0)
+    
+    # sitch composite impage
+    composite_img = compositeH(H1to2, img1, img2)
+    plt.imshow(composite_img)
+    plt.show()
+
+else:
+    print('error: not enough matches between both images, please try another pair')
